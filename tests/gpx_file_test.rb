df15b5169f1b1843ecab48d75150b1c4e49dbddf
Guillaume Virlet <github@virlet.org>
2015-07-25 12:27:21 +0200

    add support for gpx desc metadata

--- tests/gpx_file_test.rb
+++ tests/gpx_file_test.rb
@@ -9,40 +9,42 @@ class GPXFileTest < Minitest::Test
 
   def test_load_data_from_string
     gpx_file = GPX::GPXFile.new(:gpx_data => open(ONE_TRACK_FILE).read)
-	assert_equal(1, gpx_file.tracks.size)
-	assert_equal(8, gpx_file.tracks.first.segments.size)
-	assert_equal("ACTIVE LOG", gpx_file.tracks.first.name)
-	assert_equal("active_log.gpx", gpx_file.name)
-	assert_equal("2006-04-08T16:44:28Z", gpx_file.time.xmlschema)
-	assert_equal(38.681488, gpx_file.bounds.min_lat)
-	assert_equal(-109.606948, gpx_file.bounds.min_lon)
-	assert_equal(38.791759, gpx_file.bounds.max_lat)
-	assert_equal(-109.447045, gpx_file.bounds.max_lon)
+    assert_equal(1, gpx_file.tracks.size)
+    assert_equal(8, gpx_file.tracks.first.segments.size)
+    assert_equal("ACTIVE LOG", gpx_file.tracks.first.name)
+    assert_equal("active_log.gpx", gpx_file.name)
+    assert_equal("2006-04-08T16:44:28Z", gpx_file.time.xmlschema)
+    assert_equal(38.681488, gpx_file.bounds.min_lat)
+    assert_equal(-109.606948, gpx_file.bounds.min_lon)
+    assert_equal(38.791759, gpx_file.bounds.max_lat)
+    assert_equal(-109.447045, gpx_file.bounds.max_lon)
+    assert_equal('description of my GPX file with special char like &, <, >', gpx_file.description)
   end
 
   def test_load_data_from_file
     gpx_file = GPX::GPXFile.new(:gpx_file => ONE_TRACK_FILE)
-	assert_equal(1, gpx_file.tracks.size)
-	assert_equal(8, gpx_file.tracks.first.segments.size)
-	assert_equal("ACTIVE LOG", gpx_file.tracks.first.name)
-	assert_equal("active_log.gpx", gpx_file.name)
-	assert_equal("2006-04-08T16:44:28Z", gpx_file.time.xmlschema)
-	assert_equal(38.681488, gpx_file.bounds.min_lat)
-	assert_equal(-109.606948, gpx_file.bounds.min_lon)
-	assert_equal(38.791759, gpx_file.bounds.max_lat)
-	assert_equal(-109.447045, gpx_file.bounds.max_lon)
+    assert_equal(1, gpx_file.tracks.size)
+    assert_equal(8, gpx_file.tracks.first.segments.size)
+    assert_equal("ACTIVE LOG", gpx_file.tracks.first.name)
+    assert_equal("active_log.gpx", gpx_file.name)
+    assert_equal("2006-04-08T16:44:28Z", gpx_file.time.xmlschema)
+    assert_equal(38.681488, gpx_file.bounds.min_lat)
+    assert_equal(-109.606948, gpx_file.bounds.min_lon)
+    assert_equal(38.791759, gpx_file.bounds.max_lat)
+    assert_equal(-109.447045, gpx_file.bounds.max_lon)
+    assert_equal('description of my GPX file with special char like &, <, >', gpx_file.description)
   end
 
   def test_big_file
     gpx_file = GPX::GPXFile.new(:gpx_file => BIG_FILE)
-	assert_equal(1, gpx_file.tracks.size)
-	assert_equal(7968, gpx_file.tracks.first.points.size)
+    assert_equal(1, gpx_file.tracks.size)
+    assert_equal(7968, gpx_file.tracks.first.points.size)
   end
 
   def test_with_or_with_elev
     gpx_file = GPX::GPXFile.new(:gpx_file => WITH_OR_WITHOUT_ELEV_FILE)
-	assert_equal(2, gpx_file.tracks.size)
-	#assert_equal(7968, gpx_file.tracks.first.points.size)
+    assert_equal(2, gpx_file.tracks.size)
+    #assert_equal(7968, gpx_file.tracks.first.points.size)
   end
 
 end
