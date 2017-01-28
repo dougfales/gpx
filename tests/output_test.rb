d35cebc7c1688dd693d84659a2eeea8d226964e3
Nick Knipe <nick@hellodrifter.com>
2017-01-27 18:12:54 -0800

    Feature: GeoJSON conversion
    Added GPX::Geojson module for a simple conversion of GeoJSON
    features to GPX. Currently only supports LineString, Point,
    MultiLineString, and MultiPoint GeoJSON features.
    
    Note that the mapping of GeoJSON features to GPX features is not a
    clear 1-to-1 conversion, e.g., a GeoJSON LineString could map to
    either a GPX track or a GPX route. This implementation takes an
    opinionated strategy and maps each LineString as a segment of a
    GPX track. A MultiLineString maps to a GPX track, with each sub-
    MultiLineString being a GPX segment. Points and MultiPoints are
    converted to GPX waypoints.

--- tests/output_test.rb
+++ tests/output_test.rb
@@ -15,7 +15,7 @@ class OutputTest < Minitest::Test
 
 	track = Track.new(:name => "My First Track")
 	segment = Segment.new
-	
+
 	track_point_data = [
 	  {:lat => 40.036926, :lon =>-105.253487, :time => Time.parse('2005-12-31T22:01:24Z'), :elevation => 1737.24},
 	  {:lat => 40.036604, :lon =>-105.253487, :time => Time.parse("2005-12-31T22:02:01Z"), :elevation => 1738.682},
@@ -27,14 +27,14 @@ class OutputTest < Minitest::Test
 	  {:lat => 40.035274, :lon =>-105.254431, :time => Time.parse("2005-12-31T22:02:54Z"), :elevation => 1739.643},
 	  {:lat => 40.035317, :lon =>-105.254431, :time => Time.parse("2005-12-31T22:05:08Z"), :elevation => 1732.433},
 	  {:lat => 40.035317, :lon =>-105.254431, :time => Time.parse("2005-12-31T22:05:09Z"), :elevation => 1726.665}]
-	
+
 	track_point_data.each do |trk_pt_hash|
 	  segment.points <<  TrackPoint.new(trk_pt_hash)
         end
-	
+
 	track.segments << segment
 	gpx_file.tracks << track
-	
+
 	waypoint_data = [
 	  {:lat => 39.997298, :lon => -105.292674, :name => 'GRG-CA', :sym => 'Waypoint', :ele => '1766.535'},
           {:lat => 33.330190, :lon => -111.946110, :name => 'GRMPHX', :sym => 'Waypoint', :ele => '361.0981',
@@ -45,34 +45,34 @@ class OutputTest < Minitest::Test
           {:lat => 40.035301, :lon => -105.254443, :name => 'VICS',   :sym => 'Waypoint', :ele => '1535.34'},
           {:lat => 40.035301, :lon => -105.254443, :name => 'TIMEDWPT',   :sym => 'Waypoint', :ele => '1535.34', :time => Time.parse("2005-12-31T22:05:09Z")}
         ]
-	
+
 	waypoint_data.each do |wpt_hash|
 	  gpx_file.waypoints << Waypoint.new(wpt_hash)
         end
-	
+
 	route_point_data = [
 	  {:lat => 40.035467, :lon =>-105.254366, :time => Time.parse("2005-12-31T22:02:29Z"), :elevation => 1735.798},
 	  {:lat => 40.035317, :lon =>-105.254388, :time => Time.parse("2005-12-31T22:02:33Z"), :elevation => 1735.798},
 	  {:lat => 40.035274, :lon =>-105.254431, :time => Time.parse("2005-12-31T22:02:49Z"), :elevation => 1736.278} ]
-	
+
 	route = Route.new()
 	route_point_data.each do |rte_pt_hash|
 	  route.points << Point.new(rte_pt_hash)
         end
-	
+
 	gpx_file.routes << route
-	
+
 	gpx_file.write(output_file(name_of_test))
-	
-	
+
+
 	written_gpx_file = GPXFile.new(:gpx_file => output_file(name_of_test))
-	
+
 	assert_equal(File.basename(output_file(name_of_test)), written_gpx_file.name)
 	assert_equal(1, written_gpx_file.tracks.size)
 	assert_equal(1, written_gpx_file.tracks[0].segments.size)
 	assert_equal(track_point_data.size, written_gpx_file.tracks[0].segments[0].points.size)
 	assert_equal(track_point_data.size, written_gpx_file.tracks[0].points.size)
-	
+
 	# Make sure each point in the segment has the attributes it was initialized with
 	written_segment = written_gpx_file.tracks[0].segments[0]
 	track_point_data.each_with_index do |trk_pt_hash, index|
@@ -80,9 +80,9 @@ class OutputTest < Minitest::Test
 		assert_equal(value, written_segment.points[index].send(key))
 	  end
         end
-	
-	
-	
+
+
+
 	# Make sure the one route has the attributes we initialized it with
 	assert_equal(1, written_gpx_file.routes.size)
         written_route = written_gpx_file.routes[0]
@@ -92,7 +92,7 @@ class OutputTest < Minitest::Test
 		assert_equal(value, written_route.points[index].send(key))
 	  end
         end
-	
+
 	# Make sure the waypoints have all of the attributes we initialized them with
 	written_waypoints = written_gpx_file.waypoints
 	assert_equal(waypoint_data.size, written_waypoints.size)
