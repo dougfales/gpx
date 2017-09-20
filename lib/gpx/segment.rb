7ffe0b54c0e837f3f477edbcfc95d273bc8014a2
Kees Klerk <koos303@gmail.com>
2017-09-20 16:27:02 +0200

    Check if @earliest_point and @latest_point are not nil.

--- lib/gpx/segment.rb
+++ lib/gpx/segment.rb
@@ -37,8 +37,8 @@ module GPX
     def append_point(pt)
       last_pt = @points[-1]
       if pt.time
-        @earliest_point = pt if @earliest_point.nil? || (pt.time < @earliest_point.time)
-        @latest_point = pt if @latest_point.nil? || (pt.time > @latest_point.time)
+        @earliest_point = pt if @earliest_point.nil? || (@earliest_point.time && pt.time < @earliest_point.time)
+        @latest_point = pt if @latest_point.nil? || (@latest_point.time && pt.time > @latest_point.time)
       else
         # when no time information in data, we consider the points are ordered
         @earliest_point = @points[0]
